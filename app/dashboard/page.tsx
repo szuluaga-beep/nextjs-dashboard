e492b7481497285efee896e2fd262323d68a9575
szuluaga-beep <steven000069@hotmail.com>
2023-11-30 15:40:41 -0500

    fetching data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { lusitan } from '../ui/fonts'
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { Card } from '../ui/dashboard/cards';
 
-const page = () => {
+const page = async () => {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData()
+
+  // console.log(revenue)
   return (
-    <div>page</div>
+    <main>
+      <h1 className={`${lusitan.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
   )
 }
 
