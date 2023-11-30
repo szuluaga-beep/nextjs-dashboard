b483ed7409562a886489be375628963597f1de63
szuluaga-beep <steven000069@hotmail.com>
2023-11-30 12:08:19 -0500

    Agregando css, fuentes e imagenes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitan = Lusitana({ weight: ['400','700'], subsets: ['latin'] });
