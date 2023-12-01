18eea21e5ed6017dbe0744615aa6a774dfc4a770
szuluaga-beep <steven000069@hotmail.com>
2023-12-01 14:41:50 -0500

    CRUD invoices

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -8,6 +8,7 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { updateInvoice } from '@/app/lib/actions';
 import { Button } from '@/app/ui/button';
 
 export default function EditInvoiceForm({
@@ -17,8 +18,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
